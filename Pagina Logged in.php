421d7beb527603ebdaa5dec47e9dd97fdf5a4e49
victor-cuero01 <vcueroq@gmail.com>
2024-11-23 13:31:30 -0500

    Configuración para la página de inicio una ez logeado, además del boton cerrar sesión

---
+++ Pagina Logged in.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centro de desarrollo</title>
+    <link rel="stylesheet" href="./css/estilosLoggedin.css">
+
+</head>
+<body>
+    <!-- Navbar -->
+    <nav>
+        <div class="welcome">Bienvenido/a Joven Visionario</div>
+        <form action="./php/logout.php" method="POST">
+            <button type="submit" class="logout-btn">Cerrar Sesión</button>
+        </form>
+    </nav>
+</body>
+</html>
